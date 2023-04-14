61bc8fdf9c044aeed98270b6c3f3f45bf29840ee
Brandon Royce <roycebra000home@gmail.com>
2023-04-14 12:01:05 -0600

    Initital commit

---
+++ Assets/scripts/EnemyBehavior.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    private bool isGrounded;
+    private Rigidbody rb;
+    private bool collided = false;
+    private bool killMode = false;
+    public double attackDuration;
+    private double attackStart;
+    
+    public float timer;
+
+    public float pushForce;
+
+    private Vector3 targetPosition;
+
+    private bool itsTime = false;
+    private bool startTimer = false;
+
+    private bool isDashing;
+
+    private Rigidbody playerRB;
+
+    private float end;
+
+    private float time;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        time = Time.fixedTime;
+        playerRB = player.GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        isGrounded = false;
+    }
+
+    private void OnCollisionStay(Collision collisionInfo)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !isDashing)
+        {
+            killMode = true;
+            startTimer = true;
+            end = Time.time + timer;
+
+        }
+
+        if (other.gameObject.tag == "Wall")
+        {
+            isDashing = false;
+            
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerRB.AddExplosionForce(pushForce, rb.position, 10);
+            playerRB.AddForce(Vector3.up * pushForce / 3);
+            killMode = false;
+        }
+    }
+
+    void Update()
+    {
+
+        var step =  speed * Time.deltaTime; // calculate distance to move
+
+
+        
+        if (killMode == true && startTimer == true)
+        {
+            targetPosition = player.transform.position;
+            rb.transform.LookAt(targetPosition);
+        }
+        
+        
+        if (Time.time >= end && killMode == true)
+        {
+            killMode = false;
+            startTimer = false;
+            isDashing = true;
+            attackStart = Time.time;
+            rb.AddForce(transform.forward * speed);
+        }
+
+        if (isDashing)
+        {
+
+            if (Time.time > attackStart + attackDuration)
+            {
+                isDashing = false;
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+            
+            
+        }
+        
+        if (Vector3.Distance(rb.transform.position, targetPosition) < 0.01f)
+        {
+            isDashing = false;
+        }
+
+
+    }
+}
